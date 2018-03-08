0f6457dfcc7442bad99073ef26cf61d5b0a03d16
Chad Carbert <chadcarbert@me.com>
2018-03-08 13:02:55 +0100

    Working component for connection setup

--- src/ConnectionSetup.js
+++ src/ConnectionSetup.js
@@ -6,53 +6,121 @@ export default class ConnectionSetup extends Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    this.assembleTinCan();
+  }
+
   assembleTinCan() {
     const { tinCan, messageHandler } = this.props;
     tinCan
-      .setCandidateReceiver(() => {})
-      .setMessageReceiver(messageHandler || (() => {}))
-      .assemble()
-      .then(tinCan.ping)
-      .then(generatedCandidate => {
+      .setCandidateReceiver(candidate => this.receivedCandidate(candidate))
+      .setMessageReceiver(messageHandler || console.log)
+      .assemble();
+  }
+
+  receivedCandidate(candidate) {
+    if (!candidate) return;
+
+    this.setState(prevState => {
+      console.log(candidate, 'candidate received');
+      return { ...prevState, receivedCandidate: candidate };
+    });
+  }
+
+  tryCandidate() {
+    const { tinCan } = this.props;
+    const { proposedCandidate } = this.state;
+
+    tinCan.tryCandidate(proposedCandidate);
+  }
+
+  ping() {
+    const { tinCan } = this.props;
+    tinCan.ping()
+      .then(generatedOffer => {
         this.setState((prevState) => {
-          return { ...prevState, generatedCandidate };
+          return { ...prevState, generatedOffer };
         });
       });
   }
 
-  tryCandidate() {
-    console.log('will try ', this.state.proposedCandidate);
+  pinged() {
+    const { pingedOffer } = this.state;
+    const { tinCan } = this.props;
+    tinCan.pinged(pingedOffer)
+      .then(tinCan.pong)
+      .then(pongedOffer => {
+        this.setState((prevState) => {
+          return { ...prevState, pongedOffer }
+        });
+      });
   }
 
-  updateProposedCandidate(e) {
-    const candidateValue = e.target.value;
+  ponged() {
+    const { pongedOffer } = this.state;
+    const { tinCan } = this.props;
+    tinCan.ponged(pongedOffer).then(() => console.log(tinCan));
+  }
+
+  updatePingedOffer(e) {
+    const offer = e.target.value;
+
     this.setState(prevState => {
-      return { ...prevState, proposedCandidate: candidateValue };
+      return { ...prevState, pingedOffer: offer };
+    });
+  }
+
+  updatePongedOffer(e) {
+    const offer = e.target.value;
+
+    this.setState(prevState => {
+      return { ...prevState, pongedOffer: offer };
+    });
+  }
+
+  updateCandidate(e) {
+    const candidate = JSON.parse(e.target.value);
+
+    this.setState(prevState => {
+      return { ...prevState, proposedCandidate: candidate }
     });
   }
 
   render() {
-    const { generatedCandidate } = this.state;
-    const shouldDisableGenerate = typeof generatedCandidate === 'string';
+    const {
+      generatedOffer,
+      receivedCandidate,
+      pongedOffer
+    } = this.state;
+    const shouldDisableGenerate = typeof generatedOffer === 'string';
 
     return <div className="connection-setup">
-      {
-        generatedCandidate ? (
-          [
-            <div>Copy Me</div>,
-            <div><code>{ generatedCandidate }</code></div>
-          ]
-        ) : (
-          <button onClick={ () => this.assembleTinCan() }>
-            Generate Candidate
-          </button>
-        )
-      }
-
-      or
-
-      <textarea onChange={ (e) => this.updateProposedCandidate(e) }/>
-      <button onClick={ () => this.tryCandidate() }>Try Candidate</button>
-    </div>
+      <div>
+        {generatedOffer ? [<div>Copy Me</div>, <div>
+              <code>{generatedOffer}</code>
+            </div>] : <button onClick={() => this.ping()}>Ping</button>}
+        or
+        <textarea onChange={e => this.updatePingedOffer(e)} />
+        <button onClick={() => this.pinged()}>Pinged</button>
+        <br />
+        <br />
+        <br />
+        Ponged offer
+        {pongedOffer}
+        <br />
+        <br />
+        <textarea onChange={ e => this.updatePongedOffer(e) } />
+        <br />
+        <br />
+        <button onClick={ () => this.ponged() }>Ponged</button>
+      </div>
+      <div>
+        Received candidate
+        {JSON.stringify(receivedCandidate)}
+        <br />
+        <textarea onChange={ e => this.updateCandidate(e) }/>
+        <button onClick={ () => this.tryCandidate() }>Try Candidate</button>
+      </div>
+    </div>;
   }
 }
